6cdcdd2544bb47c992a3d3b114e0d0d1cdad65a4
JorgeMig <40581398+JorgeMigCM@users.noreply.github.com>
2020-12-14 19:13:02 -0400

    dynamic

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
+importScripts('assets/js/sw_utils.js');
 
 const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -53,7 +55,7 @@ self.addEventListener('activate', e => {
                 return caches.delete(key);
             }
 
-            if (key !== INMUTABLE_CACHE && key.includes('inmutable')) {
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
 
